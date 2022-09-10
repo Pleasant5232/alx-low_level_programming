e6d90732f94e806ca39acbe740644c6e57267307
Pleasant5232 <sobukolaoladayo@gmail.com>
2022-09-10 03:30:13 -0700

    Task 4-8 level C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print a book
+ *
+ * Return Always 0(Successfully)
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
