eee97c3fb89d00ffa5fbfd49300b30b7081c1ec4
Pleasant5232 <sobukolaoladayo@gmail.com>
2022-09-10 03:42:02 -0700

    Task 4-5 level C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Print a book
+ * main - Prints a text
  *
- * Return Always 0(Successfully)
+ * Return Always 0 (Success)
  */
 int main(void)
 {
-	print("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
