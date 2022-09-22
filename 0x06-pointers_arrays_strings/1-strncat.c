d2673cf5cfd66c211239c0bd6a693a3aa9449cd6
Pleasant5232 <sobukolaoladayo@gmail.com>
2022-09-22 02:43:00 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings but add inputed number of bytes
+ * _strncat - concatenate two strings but add inputted number of bytes
  * @dest: string to be appended upon
  * @src: string to be completed at the end of dest
- * @n:intwger parameter to compare index to 
+ * @n:integer parameter to compare index to 
  * Return: returns new concatenated string
  */
 
