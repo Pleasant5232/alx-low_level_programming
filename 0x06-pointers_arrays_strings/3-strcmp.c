e4ebee05eb18ac2d574b413add9e555b0e19656b
Pleasant5232 <sobukolaoladayo@gmail.com>
2022-09-22 03:29:51 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
 /**
  *  _strcmp - Function which compare two strings and
  * @s1: first string
- * @s2: second string
+ * @s2:second string
  * Return:
  * 		returns zero if s1 == s2
  * 		returns negative number if s1 < s2
@@ -12,7 +12,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i =0, diff = 0;
+	int i = 0, diff = 0;
 
 	while (1)
 	{
