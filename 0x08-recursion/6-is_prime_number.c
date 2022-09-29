be9d3f7f025a480912be6f072f52ea9015f63475
Pleasant5232 <sobukolaoladayo@gmail.com>
2022-09-29 07:49:15 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,7 +29,7 @@ int helperFunction(int num, int i)
  * is_prime_number - checks if number is prime or not.
  * @n: number to be checked.
  *
- * Return: if number is prime.
+ * Return: 1 if number is prime.
  * 0 if number is not prime.
  */
 int is_prime_number(int n)
