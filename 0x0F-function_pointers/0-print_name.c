fe6f516eddca5a7003f0f93c584da2226cab8ce5
Pleasant5232 <sobukolaoladayo@gmail.com>
2022-10-14 13:50:53 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
-#include "function_pointers.h"
 /**
- * print_name - point of entry
- * @name: name to be printed
- * @f: function pointer
- * Return: void
+ * print_name - point of entry.
+ * @name: name to be printed.
+ * @f: function pointer.
+ *
+ * Return: void.
  */
 void print_name(char *name, void (*f)(char *))
 {
